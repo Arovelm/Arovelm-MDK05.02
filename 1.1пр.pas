e977ae7c6753d0e53e71681bf49e154f75b119ca
Arovelm <Velkovroma4@gmail.com>
2026-01-13 16:34:31 +0300

    Add files via upload

---
+++ 1.1пр.pas
@@ -0,0 +1,15 @@
+﻿procedure rec(n: integer); // Параметризация 
+begin
+  // База рекурсии 
+  if n >= 0 then 
+  begin
+    print(n, ' ');
+    // Декомпозиция 
+    rec(n - 2); 
+  end;
+end;
+
+begin
+  print('Последовательность: ');
+  rec(25);
+end.
